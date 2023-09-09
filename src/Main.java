c023992f1251ae36c7f937e11d4c142a76945dd0
Poliakova75 <alla.poliakova75@gmail.com>
2023-09-09 22:11:34 +0500

    дз обьекты классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author harari = new Author("Юваль", "Харари");
+        Author pelevin = new Author("Виктор", "Пелевин");
+
+        Book sapiens = new Book("Sapiens", harari, 2011);
+        Book stuff = new Book("S.T.U.F.F", pelevin, 2010);
+
+        System.out.println("Книга 1:");
+        System.out.println("Название: " + sapiens.getTitle());
+        System.out.println("Автор: " + sapiens.getAuthor().getFirstName() + " " + sapiens.getAuthor().getLastName());
+        System.out.println("Год публикации: " + sapiens.getPublicationYear());
+
+        System.out.println("\nКнига 2:");
+        System.out.println("Название: " + stuff.getTitle());
+        System.out.println("Автор: " + stuff.getAuthor().getFirstName() + " " + stuff.getAuthor().getLastName());
+        System.out.println("Год публикации: " + stuff.getPublicationYear());
+
+        // Изменяем год публикации первой книги
+        sapiens.setPublicationYear(2005);
+
+        System.out.println("\nИзмененная книга 1:");
+        System.out.println("Название: " + sapiens.getTitle());
+        System.out.println("Автор: " + sapiens.getAuthor().getFirstName() + " " + sapiens.getAuthor().getLastName());
+        System.out.println("Год публикации: " + sapiens.getPublicationYear());
     }
 }
